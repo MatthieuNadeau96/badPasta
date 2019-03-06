ff3af8cbde27bdf47665ce7b9937cc5fd12d7b57
Matthieu <matthieunadeau96@gmail.com>
2019-03-06 12:42:17 -0500

    further implementation of firebase

--- App.js
+++ App.js
@@ -6,19 +6,79 @@ import {
   SafeAreaView,
   ScrollView,
   Image,
-  Dimensions
+  Dimensions,
+  AsyncStorage,
 } from 'react-native';
 
 import { createDrawerNavigator, createAppContainer, DrawerItems, } from 'react-navigation'
-
+import firebase from 'react-native-firebase'
 
 import HomeScreen from './src/Screens/HomeScreen'
 import SettingsScreen from './src/Screens/SettingsScreen'
 import AvatarIcon from './src/components/AvatarIcon'
 
-
-
 export default class App extends Component {
+
+  /* == Handling Permissions == */
+  async componentDidMount() {
+    this.checkPermission();
+  }
+
+  async checkPermission  ()  {
+    firebase.messaging().hasPermission()
+      .then(enabled => {
+        if (enabled) {
+          console.log('Permission granted');
+          this.getToken();
+        } else {
+          console.log('Permission request');
+          this.requestPermission();
+        }
+      });
+    // const enabled = await firebase.messaging().hasPermission();
+    // if (enabled) {
+    //     this.getToken();
+    // } else {
+    //     this.requestPermission();
+    // }
+  }
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    console.log('before fcmToken: ', fcmToken);
+    if (!fcmToken) {
+        fcmToken = await firebase.messaging().getToken();
+        if (fcmToken) {
+            // user has a device token
+            console.log('after fcmToken: ', fcmToken);
+            await AsyncStorage.setItem('fcmToken', fcmToken);
+        }
+    }
+  }
+
+  async requestPermission() {
+    // try {
+    //     await firebase.messaging().requestPermission();
+    //     // User has authorised
+    //     this.getToken();
+    // } catch (error) {
+    //     // User has rejected permissions
+    //     console.log('permission rejected');
+    // }
+
+    firebase.messaging().requestPermission()
+      .then(() => {
+        // User has authorised
+        console.log('Permission granted in requestPermission');
+        this.getToken();
+      })
+      .catch(error => {
+        // User has rejected permissions
+        console.log('permission rejected');
+      });
+  }
+  /* ========================== */
+
   render() {
     return (
       <AppContainer/>
